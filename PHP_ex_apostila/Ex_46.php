351f0091e40571abae017c1a0a27478afb196b3f
IuryCunha <iurycunha1700@gmail.com>
2025-10-21 16:56:42 -0300

    enviando ex 46 apostila

---
+++ PHP_ex_apostila/Ex_46.php
@@ -0,0 +1,11 @@
+<?php
+
+$contador = 6;
+$soma = 0;
+
+while ($contador <= 100) {
+    echo "" . $contador . PHP_EOL;
+    $contador += 2;
+    $soma += $contador;
+}
+echo "A soma dos números de 6 a 100 é: " . $soma;
