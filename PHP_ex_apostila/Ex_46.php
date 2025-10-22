ce07d74412969f21ee372ba3b05dcb1e89c260d8
IuryCunha <iurycunha1700@gmail.com>
2025-10-22 08:35:58 -0300

    atualizando ex 46 apostila

--- PHP_ex_apostila/Ex_46.php
+++ PHP_ex_apostila/Ex_46.php
@@ -5,7 +5,7 @@ $soma = 0;
 
 while ($contador <= 100) {
     echo "" . $contador . PHP_EOL;
+    $soma += $contador;
     $contador += 2;
-    $soma += $contador - 2;
 }
 echo "A soma dos números de 6 a 100 é: " . $soma;
