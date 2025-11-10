f77d3dbc7fb7ee1d9522823677f9bcd6df5fd120
IuryCunha <iurycunha1700@gmail.com>
2025-11-10 08:45:12 -0300

    Adicionando comentario

--- PHP_ex_apostila/Ex_61.php
+++ PHP_ex_apostila/Ex_61.php
@@ -1,10 +1,14 @@
 <?php
 
-$contador = 6;
+/*61) Crie um programa que mostre na tela a seguinte contagem, usando a estrutura 
+“faça enquanto” 
+0 3 6 9 12 15 18 21 24 27 30 Acabou!*/
+
+$contador = 0;
 
 do {
     echo "" . $contador . PHP_EOL;
-    $contador++;
+    $contador += 3;
 }
-while ($contador <= 11) ;
+while ($contador <= 30) ;
 echo "Acabou!";
