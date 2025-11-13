c13b1b0468c9cc05bcd6377a27047dd0fc72e8c5
IuryCunha <iurycunha1700@gmail.com>
2025-11-13 17:04:35 -0300

    Atualizando Ex

--- Projetos_pessoais/Ex_array_01.php
+++ Projetos_pessoais/Ex_array_01.php
@@ -1,5 +1,7 @@
 <?php 
 
+//Arrays numéricos
+
 $carros = array(1=>"BMW", 2=>"Veloster", 3=>"Hilux");
 $carros[] = "Amarok";
 $carros[10] = "Camaro";
@@ -17,3 +19,51 @@ echo PHP_EOL;
 
 $clientes = ["Rodrigo", "Felipe", "Bia"];
 print_r($clientes);
+
+//Count
+
+$TotalClientes = count($clientes);
+echo $TotalClientes;
+echo PHP_EOL;
+
+//Foreach
+
+foreach ($clientes as $valor) {
+    echo $valor . PHP_EOL;
+}
+
+echo "______________________________________________________________________________________________________". PHP_EOL;
+
+// Arrays associativos
+
+$pessoa = array("Nome" => "Rodrigo", "idade" => 23, "altura" => 1.75);
+$pessoa["cidade"] = "Itabuna";
+print_r($pessoa);
+
+foreach($pessoa as $indice => $valor) {
+    echo $indice .":". $valor . PHP_EOL; 
+}
+echo "_____________________________________________________________________________________________________" . PHP_EOL;
+// Arrays multidimensionais
+
+$times = array(
+        "Cariocas" => array("Campeao"=>"Vasco", "Vice"=>"Flamengo", "Terceiro"=>"Botafogo"),
+        "Paulistas" => array("Campeao"=>"Santos", "Vice"=>"SãoPaulo", "Terceiro"=>"Palmeiras"),
+        "Baianos" => array("Campeao"=>"Bahia", "Vice"=>"Vitória", "Terceiro"=>"Itabuna")
+        );
+echo $times ["Cariocas"]["Campeao"];
+echo PHP_EOL;
+
+foreach ($times["Cariocas"] as $indice => $valor) {
+    echo $indice .":".$valor. PHP_EOL; 
+}
+echo "_____________________________________________________________________________________________________" . PHP_EOL;
+
+foreach ($times["Baianos"] as $indice => $valor) {
+    echo $indice .":".$valor. PHP_EOL; 
+}
+echo "_____________________________________________________________________________________________________" . PHP_EOL;
+
+foreach ($times["Paulistas"] as $indice => $valor) {
+    echo $indice .":".$valor. PHP_EOL; 
+}
