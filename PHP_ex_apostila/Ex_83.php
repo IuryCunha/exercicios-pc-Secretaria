a859f83b2a91db20b92d5d294f0e1349727b98a2
IuryCunha <iurycunha1700@gmail.com>
2025-11-25 09:12:09 -0300

    enviando ex 83 apostila

--- PHP_ex_apostila/Ex_83.php
+++ PHP_ex_apostila/Ex_83.php
@@ -5,3 +5,21 @@ aleatórios (entre 0 e 99) gerados pelo computador. Logo em seguida, mostre os
 números gerados e depois coloque o vetor em ordem crescente, mostrando no final 
 os valores ordenados. */
 
+$vetores = [];
+$vetorOrdena = [];
+
+for ($i = 0; $i < 5; $i++) {
+    $vetores [$i] = rand(1,7);
+}
+
+echo "Valores aleatórios: ";
+foreach ($vetores as $valor) {
+    echo $valor . " ";
+}
+echo PHP_EOL;
+
+sort($vetores);
+echo "Valores ordenados: ";
+foreach ($vetores as $valorOrdena) {
+    echo $valorOrdena . " ";
+}
