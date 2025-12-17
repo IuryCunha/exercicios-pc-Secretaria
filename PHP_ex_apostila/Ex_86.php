60be861f14be1a6558bf1a983bc471a3f3d30b0c
IuryCunha <iurycunha1700@gmail.com>
2025-12-17 08:50:26 -0300

    Enviando ex 86 apostila

--- PHP_ex_apostila/Ex_86.php
+++ PHP_ex_apostila/Ex_86.php
@@ -7,3 +7,19 @@ Ex: Ao chamar Gerador() aparece:
 Olá, Mundo!         
 +-------=======------+ */
 
+function mensagem($Texto) {
+    return "Olá," . $Texto . "!";
+}
+echo mensagem("Mundo");
+
+echo PHP_EOL;
+
+//Acima temos uma função, que utiliza-se return.
+//Abaixo temos um procedimento, que não se faz uso de return.
+
+function Gerador() {
+    echo "+-------=======------+" . PHP_EOL;
+    echo "Olá, Mundo!" . PHP_EOL;
+    echo "+-------=======------+" . PHP_EOL;
+}
+Gerador();
