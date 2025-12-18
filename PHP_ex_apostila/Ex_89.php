ad49a87b474d37142d9e71ad652b9a794e4b6f8e
IuryCunha <iurycunha1700@gmail.com>
2025-12-18 08:43:36 -0300

    enviando ex 89 apostila

---
+++ PHP_ex_apostila/Ex_89.php
@@ -0,0 +1,32 @@
+<?php
+
+/*89) Crie um programa que melhore o procedimento Gerador() da questão anterior 
+para que o programador possa escolher uma entre três bordas: 
++-------=======------+ Borda 1 
+~~~~~~~~:::::::~~~~~~~ Borda 2 
+<<<<<<<<------->>>>>>> Borda 3 
+Ex: Uma chamada válida seria Gerador("Portugol Studio", 3, 2) 
+~~~~~~~~:::::::~~~~~~~ 
+Portugol Studio 
+Portugol Studio 
+Portugol Studio 
+~~~~~~~~:::::::~~~~~~~ */
+
+$bordas = [
+    1 =>   '+-------=======------+',
+    2 =>   '~~~~~~~~:::::::~~~~~~~',
+    3 =>   '<<<<<<<<------->>>>>>>' 
+];
+
+function Gerador($texto, $qtd, $tipoBorda, $bordas) {
+    echo $bordas[$tipoBorda] . PHP_EOL;
+
+    for ($i = 0; $i < $qtd; $i++) {
+        echo $texto . PHP_EOL;
+    }
+
+    echo $bordas[$tipoBorda] . PHP_EOL;
+}
+Gerador("Portugol Studio", 3, 3, $bordas);
+
+/* o array $bordas está dentro da função para que ela seja vista, caso contrário ela passa a não existir para a função/*
