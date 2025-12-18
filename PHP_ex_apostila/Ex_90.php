f8728528f0946587dcca5feb6c51d1249a545ce7
IuryCunha <iurycunha1700@gmail.com>
2025-12-18 11:09:38 -0300

    enviando ex 90 apostila

--- PHP_ex_apostila/Ex_90.php
+++ PHP_ex_apostila/Ex_90.php
@@ -1,2 +1,20 @@
 <?php
 
+/*90) Desenvolva um algoritmo que leia dois valores pelo teclado e passe esses 
+valores para um procedimento Somador() que vai calcular e mostrar a soma entre 
+eles. */
+
+
+$valores = []; // Cria um array vazio
+
+for ($i = 0; $i < 2; $i++) {
+    echo "Digite um valor: " . PHP_EOL;
+    $valores[$i] = (int) readline(); // Lê dois números do usuário, guarda os números no array
+}
+
+function Somador($valores) { // Envia o array para a função
+    $soma = $valores[0] + $valores[1]; // A função soma os valores
+    echo "A soma dos valores é $soma" . PHP_EOL;
+}
+
+Somador($valores); // Mostra o resultado
